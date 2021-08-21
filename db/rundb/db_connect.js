9a59551ff7edc6cd2bc2a223bf07d211aa5831cf
Anton <golubin1990@gmail.com>
2021-08-21 13:41:06 -0400

    Added dotenv to the file and export of pool database

--- db/rundb/db_connect.js
+++ db/rundb/db_connect.js
@@ -1,3 +1,5 @@
+// amodule to connect to the database using variables in process.env
+require('dotenv').config()
 const pg = require('pg');
 const {Pool} = pg.Pool;
 
@@ -8,3 +10,11 @@ const config = {
   passowrd: process.env.DB_PASS,
   port:5432
 }
+
+const pool = new Pool(config);
+
+pool.connect(() => {
+  console.log('connected to database')
+});
+
+module.export = pool;
