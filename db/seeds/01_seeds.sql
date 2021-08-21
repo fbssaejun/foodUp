01313cdb5e4d7eadc587def0081d7170afef628d
Anton <golubin1990@gmail.com>
2021-08-21 15:06:48 -0400

    Start filling up database with test data

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,23 @@
+-- Users table seeds here (Example)
+INSERT INTO users (name, email, password, phone_number, customer, country, city, street, province, post_code)
+  VALUES('Irene Vega','reaganshaffer@live.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856987, TRUE, )
+  VALUES('Georgie Sutton', 'alexandraellis@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856988, TRUE, )
+  VALUES('Betty Lowe', 'sebastianmerritt@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856989, TRUE, )
+  VALUES('Mayme Vega', 'williamhouse@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856991, TRUE, )
+  VALUES('Ernest Elliott', 'violetsteele@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856992, TRUE, )
+  VALUES('Eugene Graves', 'josiahstanley@google.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856993, TRUE, )
+  VALUES('Eugene Graves', 'thomasbaird@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856994, TRUE, )
+  VALUES('Miguel Pierce', 'cooperwright@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856995, TRUE, )
+  VALUES('Mary Montgomery', 'chloemerrill@google.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856996, TRUE, )
+  VALUES('Logan Allison', 'adammcmahon@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856997, TRUE, )
+  VALUES('Stephen Nichols', 'charliemacdonald@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856998, TRUE, )
+  VALUES('Brian Miles', 'carsono''connor@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856999, TRUE, )
+  VALUES('Violet Parks', 'elijahwiggins@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856901, TRUE, )
+  VALUES('Edna Mack', 'declanrhodes@google.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856902, TRUE, )
+  VALUES('Jacob Houston', 'joshuamccarthy@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856903, TRUE, )
+  VALUES('Peter Holmes', 'tylerromero@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856904, TRUE, )
+  VALUES('Eric Stevenson', 'carsonbowers@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856905, TRUE, )
+  VALUES('Vernon Miles', 'alainarich@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 456856906, FALSE, )
+
+
+
