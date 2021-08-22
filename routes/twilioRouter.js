fe806320fe2e87acaa84596db75f4c2fbb79e83f
Anton <golubin1990@gmail.com>
2021-08-22 16:44:32 -0400

    Moved twilio initialization to a seperate file to promote modularity

--- routes/twilioRouter.js
+++ routes/twilioRouter.js
@@ -1,21 +1,16 @@
 //Require dotenv module to process environment variables in .env file:
 require('dotenv').config()
-//Connect twilio module
-const twilio = require('twilio');
-//Set accountSID using .env file:
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-//Set a token using .env file:
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+//Connect twilio library
+const client = require('../lib/twilio')
 //SET a recepient phone-number:
 const recPhoneNumber = process.env.TARGET_NUMBER;
 //SET twilio phone-number:
 const twilioNumber = process.env.TWILIO_NUMBER;
-//Creating an instance of twilio class
-const client = new twilio(accountSid, authToken);
+
 
 client.messages
   .create({
-    body: 'Hello from Node',
+    body: 'Hello from Node 2',
     to: recPhoneNumber,
     from: twilioNumber
   })
