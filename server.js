529bdbf39828c7613ee6d35587c33030c7720dce
Anton <golubin1990@gmail.com>
2021-08-22 11:13:17 -0400

    Add a router to be used for testing data/queries

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const morgan     = require('morgan');
 const userRoutes = require("./routes/userRoutes");
 const ownerRoutes = require("./routes/ownerRoutes");
 
+//Test Route
+const testRoutes = require("./routes/testRoutes");
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 // The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -38,6 +41,18 @@ ownerRoutes(ownerRouter);
 //Middleware assinged
 app.use("/admin", ownerRouter);
 
+//=================================================================================================================================================
+//=================================================    Routing for test/ paths  !!!FOR TEST ONLY!!!   =============================================
+//=================================================================================================================================================
+// Create a new instance of a router which will be used for OWNER routes
+const testRouter = express.Router();
+// Pass this routers into ownerRoutes function which was imported from route/ownerRouter.js. This function will mutate ownerRouter object
+// and mount it to routes defined in ownerRouter file. Object is mutated by reference, so return value does not need to be explicitly assigned.
+testRoutes(testRouter);
+//Middleware assinged
+app.use("/test", testRouter);
+
+
 //===============================================================================================================================
 //=================================================    Routing for user / paths     =============================================
 //===============================================================================================================================
@@ -48,6 +63,7 @@ const userRouter = express.Router();
 userRoutes(userRouter);
 //Middleware assinged
 app.use("/", userRouter);
+
 
 
 app.listen(PORT, () => {
