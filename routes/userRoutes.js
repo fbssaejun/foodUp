d33b511048a6b0d49fd6d035d22cba7b7c611085
Anton <golubin1990@gmail.com>
2021-08-21 20:47:35 -0400

    Added a new route for /menu

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 // const bcrypt = require('bcrypt');
-const {getMenuItemsForClients, getClientOrders} = require('../db/rundb/client_queries')
+const {getMenuItemsForClients, getClientOrders} = require('../db/rundb/client_queries.js')
 
 // PG database client connection
 
 module.exports = function(router) {
 
   router.get('/menu', (req, res) => {
-    getMenuItemsForClients.
-      then((result) => console.log(result));
+    getMenuItemsForClients()
+    .then((items) => res.json(items))
+    .catch((error) => res.status(404).send("Error: something went wrong: ", error.message))
   })
 
 
-
+  return router;
 }
