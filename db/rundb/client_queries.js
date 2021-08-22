126025eaa676267fd533c5c83fb2b8c0fadb3015
Anton <golubin1990@gmail.com>
2021-08-21 21:10:23 -0400

    Add a function for query which returns all users

--- db/rundb/client_queries.js
+++ db/rundb/client_queries.js
@@ -10,11 +10,15 @@ const getMenuItemsForClients = function() {
   return pool.query(queryString)
     .then(result => result.rows)
     .catch(error => console.log(error.message));
-}
+};
 
 exports.getMenuItemsForClients = getMenuItemsForClients;
 
-
+/**
+ * Get a list of all items from the database which user can see.
+ * @param  {Integer} id An id of user
+ * @return {Promise<{}>} A promise to the user.
+ */
 const getClientOrders = function(id) {
   const queryString = 'SELECT * FROM orders'
   if (id !== undefined) {
@@ -22,10 +26,28 @@ const getClientOrders = function(id) {
   }
 
   return pool.query(queryString, [id])
-    .then(result => result.rows)
+    .then(result => result.rows[0])
     .catch(error => console.error(error.message));
+};
+
+exports.getClientOrders = getClientOrders
+
+/**
+ * Get a list of all users from the database.
+ * @return {Promise<{}>} A promise to the user.
+ */
+
+const getUsers = function() {
+  const queryString = `SELECT * FROM users`
+  return pool
+    .query(queryString)
+    .then(result => result.rows)
+    .catch(error => console.error(error.message))
 }
 
-module.export = {
-  getClientOrders
-}
+exports.getUsers = getUsers
+
+
+
+
+
