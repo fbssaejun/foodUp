5798aa30a4f6a8c79e9251a8e10d671207fee987
Anton <golubin1990@gmail.com>
2021-08-21 22:23:24 -0400

    Add a new route to ownerRoutes.js

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,5 +1,21 @@
-const {getMenuItemsForClients, getClientOrders, getUsers} = require('../db/rundb/client_queries.js')
+const {getMenuItems,getAllOrders,getMenuItem, getOrderDetails} = require('../db/rundb/owner_queries.js')
 
 module.exports = function(router) {
+
+  router.get('/menu/:item', (req, res) => {
+    const itemID = req.params.item;
+    getMenuItem(itemID)
+      .then(data => {
+        res
+          .status(200)
+          .json(data)
+      })
+      .catch(error => {
+         res
+            .status(500)
+            .send("Catch :", error.message)
+      });
+  });
+
   return router;
 }
