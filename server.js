7b677615497e91e28088e1cf9ab6f3d6e3931a0e
Anton <golubin1990@gmail.com>
2021-08-21 21:32:27 -0400

    Removed all routing from the main file. Added an initiation to routes for Owner

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ require('dotenv').config();
 
 // Web server config
 const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const userRoutes = require('./routes/userRoutes')
 const express    = require("express");
 const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
-
-// PG database client/connection setup
+// Define a source for Routes to Mount for each Resource
+const userRoutes = require("./routes/userRoutes");
+const ownerRoutes = require("./routes/ownerRoutes");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
@@ -28,35 +27,27 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
-// Define Routes for each Resource
-const usersRoutes = require("./routes/userRoutes");
-
-
-
-//=============================
-//==Routing for user / paths ==
-//=============================
-// Create a new instance of a router which will be used for user routes
+//===============================================================================================================================
+//=================================================    Routing for user / paths     =============================================
+//===============================================================================================================================
+// Create a new instance of a router which will be used for USER routes
 const userRouter = express.Router();
 // Pass this routers into userRoutes function which was imported from route/userRouter.js. This function will mutate userRouter object
-// and mount it to routes defined in userROuter file. Object is mutated by reference, so return value does not need to be explicitly assigned
-userRoutes(userRouter)
+// and mount it to routes defined in userROuter file. Object is mutated by reference, so return value does not need to be explicitly assigned.
+userRoutes(userRouter);
 //Middleware assinged
 app.use("/", userRouter);
 
+//===============================================================================================================================
+//=================================================    Routing for user / paths     =============================================
+//===============================================================================================================================
+// Create a new instance of a router which will be used for OWNER routes
+const ownerRouter = express.Router();
+// Pass this routers into ownerRoutes function which was imported from route/ownerRouter.js. This function will mutate ownerRouter object
+// and mount it to routes defined in ownerRouter file. Object is mutated by reference, so return value does not need to be explicitly assigned.
+ownerRoutes(ownerRouter);
+app.use("/admin", ownerRouter);
 
-
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/customer_menu", (req, res) => {
-  res.render("customer_menu");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
