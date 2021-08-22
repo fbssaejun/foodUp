d50273e4bd86fddb4da13b3260f91721db83bfca
Anton <golubin1990@gmail.com>
2021-08-21 20:47:12 -0400

    Change the way functions are exported

--- db/rundb/client_queries.js
+++ db/rundb/client_queries.js
@@ -1,25 +1,31 @@
-const db = require('./db_connect');
+const pool = require('./db_connect');
 
-// Get all items which are available for menu
-const getMenuItemsForClients = () => {
+
+/**
+ * Get a list of all items from the database which user can see.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getMenuItemsForClients = function() {
   const queryString = 'SELECT * FROM menu_items WHERE available'
-  return db.query(queryString)
+  return pool.query(queryString)
     .then(result => result.rows)
     .catch(error => console.log(error.message));
 }
 
-const getClientOrders = (id) => {
+exports.getMenuItemsForClients = getMenuItemsForClients;
+
+
+const getClientOrders = function(id) {
   const queryString = 'SELECT * FROM orders'
   if (id !== undefined) {
     queryString += ` WHERE id = $1`
   }
 
-  return db.query(queryString, [id])
+  return pool.query(queryString, [id])
     .then(result => result.rows)
     .catch(error => console.error(error.message));
 }
 
 module.export = {
-  getMenuItemsForClients,
   getClientOrders
 }
