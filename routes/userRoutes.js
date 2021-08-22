b13e136cd81fea28e2587d3288260adb8e8e5a3c
Anton <golubin1990@gmail.com>
2021-08-21 21:11:02 -0400

    Add a new router to /users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 // const bcrypt = require('bcrypt');
-const {getMenuItemsForClients, getClientOrders} = require('../db/rundb/client_queries.js')
+const {getMenuItemsForClients, getClientOrders, getUsers} = require('../db/rundb/client_queries.js')
 
 // PG database client connection
 
@@ -7,10 +7,27 @@ module.exports = function(router) {
 
   router.get('/menu', (req, res) => {
     getMenuItemsForClients()
-    .then((items) => res.json(items))
-    .catch((error) => res.status(404).send("Error: something went wrong: ", error.message))
-  })
+    .then((items) => {
+      res
+      .json(items)})
+    .catch((error) => {
+      res
+      .status(500)
+      .send("Error: something went wrong: ", error.message)})
+  });
 
+  router.get("/users", (req, res) => {
+    getUsers()
+      .then(data => {
+        res
+        .json(data);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json(error.message);
+      });
+  });
 
   return router;
 }
