79599833cb9763966f5721cf9815a5cb1448a9dc
Anton <golubin1990@gmail.com>
2021-08-23 14:55:54 -0400

    Add a script file for login/register form

---
+++ public/scripts/login-register.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+ // Catch default event handler for sign-up Button on login/register form,
+ // supress it and replace with a custom one made with Ajax
+
+  $("#sign-in").submit(function(event){
+      event.preventDefault();
+      const login = $(this).children('input').first().val()
+      const password =$(this).children('input').eq(1).val()
+
+  });
+
+});
