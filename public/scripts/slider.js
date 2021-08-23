56f65bce936268e7260188aabf1a94536d143b35
Anton <golubin1990@gmail.com>
2021-08-22 22:42:59 -0400

    Add jquery logic to slider login/password page

---
+++ public/scripts/slider.js
@@ -0,0 +1,64 @@
+// Get dimensions of footer and header to adjust toggable login/register form
+  //Get header dimensions
+const getNavSize = function() {
+  let elementTop = $("nav").offset().top;
+  let elementBottom = elementTop + $("nav").outerHeight();
+  return elementBottom;
+}
+
+const getFooterSize = function() {
+  let elementBottom = $("footer").offset().top;
+  return elementBottom;
+}
+
+
+$(document).ready(function(){
+
+  $("a[name='login']").on('click', (event)=> {
+    event.preventDefault();
+    const navHeight = getNavSize();
+    const footerStart = getFooterSize();
+    const startLogin = navHeight;
+    const heightLogin = footerStart - navHeight;
+    $("#sidebar-wrapper").css('top', navHeight)
+    $("#sidebar-wrapper").css('height', heightLogin)
+    $("#sidebar-wrapper").css('display', 'block');
+  });
+
+  $(".to-signin").on("click", function () {
+    $(this)
+      .addClass("top-active-button")
+      .siblings()
+      .removeClass("top-active-button");
+    $(".form-signup").slideUp(500);
+    $(".form-signin").slideDown(500);
+  });
+
+  $(".to-signup").on("click", function () {
+    $(this)
+      .addClass("top-active-button")
+      .siblings()
+      .removeClass("top-active-button");
+    $(".form-signin").slideUp(500);
+    $(".form-signup").slideDown(500);
+  });
+
+  $(".to-signin-link").on("click", function () {
+    $(".to-signin")
+      .addClass("top-active-button")
+      .siblings()
+      .removeClass("top-active-button");
+    $(".form-signup").slideUp(200);
+    $(".form-signin").slideDown(200);
+  });
+
+  $(".to-signup-link").on("click", function () {
+    $(".to-signup")
+      .addClass("top-active-button")
+      .siblings()
+      .removeClass("top-active-button");
+    $(".form-signin").slideUp(200);
+    $(".form-signup").slideDown(200);
+  });
+
+});
