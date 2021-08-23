96bb90b92ddbc271fd5efd7fb4e2fcf40e8ad100
Anton <golubin1990@gmail.com>
2021-08-23 17:13:55 -0400

    added password check to login fuction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,8 @@
 // const bcrypt = require('bcrypt');
-const {getMenuItemsForClients, getClientOrders, getUsers} = require('../db/rundb/client_queries.js')
+const {getMenuItemsForClients, getClientOrders, getUsers} = require('../db/rundb/client_queries.js');
+const {checkUserExists, getUserPassword} = require('../db/rundb/login_queries.js');
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 
 module.exports = function(router) {
 
@@ -40,13 +43,36 @@ module.exports = function(router) {
   ====================================================== POST ROUTES  ====================================================================
   =======================================================================================================================================*/
 
+  ///Helper function for a promise:
+
+  const checkPassword = function(check, login, userpassword) {
+    console.log("Here is check value:", check);
+    if (Number(check) === 1) {
+      console.log("I AM HERE")
+      getUserPassword(login)
+      .then((systemPassword) => {
+        console.log("Userpassowrd:", userpassword);
+        console.log("System Password:", systemPassword);
+        console.log(bcrypt.compareSync(userpassword, systemPassword['password']))
+      }) ;
+    }
+    // else {
+    //   res
+    //     .status(404)
+    //     .send("User not found")
+    // }
+  }
+
+
   router.post('/login', (req, res) => {
-    console.log('Here i am:', req.body)
     const {login, password} = req.body;
-
-
-
-  })
+    checkUserExists(login)
+      .then((data) => data['count'])
+      .then((data) => {
+        checkPassword(data, login, password)
+      })
+      // .catch((error) => res.status(404).send("Error: ", error.message));
+    })
 
 
   return router;
