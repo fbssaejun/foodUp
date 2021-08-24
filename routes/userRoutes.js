bbca308a13d64a12247fe79c628bc991e2bdfbd5
Anton <golubin1990@gmail.com>
2021-08-23 20:03:00 -0400

    Move login-registration routers from userRoutes to a separate file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,7 @@
 // const bcrypt = require('bcrypt');
 const {getMenuItemsForClients, getClientOrders, getUsers} = require('../db/rundb/client_queries.js');
-const {checkUserExists, getUserPassword, getUserID} = require('../db/rundb/login_queries.js');
-const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session');
+const loginRegisterRoutes = require('./loginRegisterRoutes.js')
+// const bcrypt = require('bcrypt');
 
 module.exports = function(router) {
 
@@ -40,48 +39,10 @@ module.exports = function(router) {
 
 
   /* =====================================================================================================================================
-  ====================================================== POST ROUTES  ====================================================================
+  ====================================================== LOGIN/REGISTRATION ROUTES =======================================================
   =======================================================================================================================================*/
-
-  ///Helper function for a promise:
-
-  const checkPassword = function(check, login, userpassword) {
-    if (Number(check) === 1) {
-      return getUserPassword(login)
-      .then((systemPassword) => {
-        return (bcrypt.compareSync(userpassword, systemPassword['password']))
-      }) ;
-    }
-  }
-
-  //Login route which will redirect user to customer_page if his attempt is succesfull
-
-  router.post('/login', (req, res) => {
-    const {login, password} = req.body;
-    checkUserExists(login)
-      .then((data) => data['count'])
-      .then((data) => {
-        checkPassword(data, login, password)
-          .then((result) => {
-            if (result) {
-              getUserID(login)
-              .then((id)=> {
-                userID = id;
-                req.session.userid = userID.id;
-                console.log(req.session)
-                res
-                .status(200)
-                .json({ success: true });
-              });
-            } else {
-              res
-                .status(401)
-                .json({ success: false });
-            }
-          })
-      })
-      .catch((error) => res.status(404).send("Error: ", error.message));
-  })
+  // Mutate router object by adding routes for login/registration
+  loginRegisterRoutes(router);
 
   return router;
 }
