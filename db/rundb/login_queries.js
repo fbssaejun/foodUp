4a0affd6a31454004c39028993d3adea360fa026
Anton <golubin1990@gmail.com>
2021-08-24 10:35:51 -0400

    Fix querty to insert a new user in the database

--- db/rundb/login_queries.js
+++ db/rundb/login_queries.js
@@ -7,8 +7,8 @@ const pool = require('./db_connect');
  * @return {Promise<{}>} A promise to the user.
  */
 const checkUserExists = function(login) {
-  const email = String(login);
 
+  const email = String(login);
   const queryString = `SELECT count(*) FROM users WHERE email LIKE $1`;
   return pool.query(queryString, [email])
     .then(result => result.rows[0])
@@ -46,3 +46,24 @@ exports.getUserPassword = getUserPassword;
 }
 
 exports.getUserID  = getUserID ;
+
+
+/**
+ * Returns user ID
+ * @param  {string} email a user email address
+ *  @param  {hashed string} password a user email address
+ * @return {Promise<{}>} A promise to the user with password for specified email
+ */
+ const addUser = function(login, password) {
+  const vals = [];
+  vals.push(String(login));
+  vals.push(String(password));
+  const queryString = `INSERT INTO users(name, email, password, phone_number, customer, province,  city, country, street, post_code)
+                          VALUES('placeholder', $1, $2, 'placeholder', TRUE, 'placeholder', 'placeholder', 'placeholder', 'placeholder', 'placeholder')
+                          RETURNING *`;
+  return pool.query(queryString, vals)
+    .then(result => {console.log(result.rows)})
+    .catch(error => console.log(error.message));
+}
+
+exports.addUser  = addUser;
