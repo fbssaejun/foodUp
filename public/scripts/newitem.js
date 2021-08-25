817b5fa70ab4fc77cda1eb314d620d79dafd9bb2
Anton <golubin1990@gmail.com>
2021-08-25 17:20:31 -0400

    Add a script for creating a new item and added picture preview functionality

--- public/scripts/newitem.js
+++ public/scripts/newitem.js
@@ -1,4 +1,6 @@
 
+// const loadDashboardMenu = require('./dashboard')
+
 const $form = $(`
   <form id = "edit-form">
   <div><label>Enter Name</label><input id = "name" value = ""></input></div>
@@ -12,13 +14,25 @@ const $form = $(`
   <div><label for="False">Hide on the Menu</label>
   <input type="radio" value="value2" name="group1" id = "False"></div>
   </fieldset></div>
-  <div><button type="submit" class = "submit-edit" id ="edit-submit">Create</button><button type="submit" class = "submit-edit" id = "cancel">Cancel</button><div>
+  <div id = 'button-storage'><button type="submit" class = "submit-edit" id ="edit-submit">Create</button><button type="submit" class = "submit-edit" id = "cancel">Cancel</button><div>
   </div>
-  <div id = "create image"><div>
+  <div id = "pic-store">
+  <img src="" alt="No Picture Loaded" id = "picture">
+  </div id = "pic-store">
   </form>
 `)
 
+const fadeAway = function() {
+  $form.fadeOut("slow");
+  $('#hideclass').fadeIn("slow");
+  $('.dashboard-2').fadeIn("slow");
+  $('#close-item').fadeOut("slow");
+  $('#close-item').css('display', 'flex');
+  $('#add-item').fadeIn("slow");
+}
+
 $(document).ready(function () {
+
   $('#add-item').on('click', (event) => {
 
     console.log(event)
@@ -32,13 +46,52 @@ $(document).ready(function () {
   })
 
   $('#close-item').on('click', (event) => {
-    $form.fadeOut("slow");
-    $('#hideclass').fadeIn("slow");
-    $('.dashboard-2').fadeIn("slow");
-    $('#close-item').fadeOut("slow");
-    $('#close-item').css('display', 'flex');
-    $('#add-item').fadeIn("slow");
+    fadeAway();
   })
 
-
+  $form.on('submit', (event) => {
+    event.preventDefault();
+    if (event.originalEvent.submitter.id === "cancel"){
+      fadeAway();
+    } else if (event.originalEvent.submitter.id === "edit-submit"){
+      const Name = $form.find('#name').val();
+      const Price = $form.find('#price').val();
+      const Calories = $form.find('#calories').val();
+      const Cuisine = $form.find('#cuisine').val();
+      const Picture = $form.find('#image').val();
+      let Availability;
+      if ($('#True').is(":checked")) {
+         Availability = true;
+      } else {
+        Availability = false;
+      }
+      const request = {
+        Name,
+        Price,
+        Calories,
+        Cuisine,
+        Picture,
+        Availability
+      }
+      $.ajax({
+        type: "POST",
+        url: `../api/menu/create/item/`,
+        data: request,
+      })
+      .then(() => {
+      fadeAway();
+      loadDashboardMenu()});
+    }
+  });
+  const $image = $form.find('#image')
+  $image.on("paste", function(){
+    var element = this;
+  setTimeout(function () {
+    var text = $(element).val();
+    $image.attr("src", text);
+    $("#pic-store").empty();
+    const $newImage = `<img src=${text} alt="No Picture Loaded" id = "picture"></img>`
+    $("#pic-store").append($newImage);
+  }, 100);
+  })
 });
