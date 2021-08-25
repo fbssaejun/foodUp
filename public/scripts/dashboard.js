e25711085b40b976993f5aa1ed44e2adbb2dfc1e
Anton <golubin1990@gmail.com>
2021-08-25 12:07:17 -0400

    Added a styling for edit menu item dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -60,12 +60,16 @@ const renderItems = (data) => {
 };
 
 const createMenuElement =function(item){
-let status = "NO"
+let status = "NO";
+let checkBoxStatus = "unchecked";
 if (item.available) {
   status = "YES";
+  checkBoxStatus = "checked";
 }
+const $first = $('<div>').addClass('product');
 const $line = $(`
-            <form>
+            <form id = "menu-box">
+
             <div id = "name">${item.name}</div>
              <div id = "price">$ ${item.price}</div>
              <div id ="calories">${item.kalories}</div>
@@ -73,18 +77,58 @@ const $line = $(`
              <div id = "status">${status}</div>
              <button type="submit" id = "edit">Edit</button>
              <button type="submit" id = "delete">Delete</button>
-             </div>
+
              </form>
       `)
-    console.log($line)
+      const $edit = $(`
+         <form id = "edit-form">
+         <div><label>Edit Name</label><input id = "name" value = "${item.name}"></input></div>
+         <div><label>Edit Price</label><input id = "price" value = ${item.price}></input></div>
+         <div><label>Edit Calories</label><input id ="calories" value = ${item.kalories}></input></div>
+         <div><label>Edit Cuisine</label><input id ="cuisine" value = "${item.cuisine}"></input></div>
+         <div><label>Edit Picture</label><input id = "image" value = "${item.image_url}"></input></div>
+         <div> <fieldset id = "group1">
+         <div><label for="True">Show On the Menu</label>
+         <input type="radio" value="value1" name="group1" id="True" checked></div>
+         <div><label for="False">Hide on the Menu</label>
+         <input type="radio" value="value2" name="group1" id = "False"></div>
+         </fieldset></div>
+         <div><button type="submit" class = "submit-edit" id ="edit-submit">Edit</button><button type="submit" class = "submit-edit" id = "cancel">Cancel</button><div>
+         </div>
+         </form>
+  `)
 
+
+
+    //SHOW EDIT FORM
     $line.on('submit', (event) => {
       event.preventDefault();
-      console.log(item.name);
-      console.log((event));
+      if (event.originalEvent.submitter.id === "edit") {
+        $("#edit-form").slideDown("slow");
+      } else {
+          console.log("This is delete!")
+        }
+
     });
 
-    return $line;
+    $edit.on('submit', (event) => {
+      event.preventDefault();
+      console.log($edit.find('#name').val())
+
+      if (event.originalEvent.submitter.id === "cancel"){
+        $edit.slideUp("slow");
+      } else {
+        const newName = $edit.find('#name').val();
+        const newPrice = $edit.find('#price').val();
+        const newCalories = $edit.find('#price').val();
+        const newCuisine = $edit.find('#cuisine').val();
+        const newPicture = $edit.find('#image').val();
+
+      }
+
+    })
+
+    return $first.append($line, $edit);
 
   };
 
