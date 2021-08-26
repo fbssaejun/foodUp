35348635e4f4ca370f1bb560683484c68db435e2
Anton <golubin1990@gmail.com>
2021-08-25 20:15:33 -0400

    Add a new route to owner to send textes

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,5 +1,6 @@
 const {getMenuItems,getAllOrders,getMenuItem, getOrderDetails} = require('../db/rundb/owner_queries.js');
 const {getUserStatus} = require('../db/rundb/login_queries.js');
+const {sendText}  =require('./twilioRouter');
 
 //user this user for owner alainarich@aol.com
 
@@ -53,6 +54,28 @@ module.exports = function(router) {
         .send("Catch :", error.message)
     })
   });
+
+  router.post('/sendtext', (req, res) => {
+    const {text} = req.body;
+    const sessionId = req.session.userid;
+    getUserStatus(sessionId)
+      .then(() =>{
+        if (sessionId) {
+          sendText(text)
+            .then((data) => {
+              res
+              .status(200)
+              .json(data)
+            })
+            .catch(error => console.error(error.message))
+        } else {
+          res
+          .status(403)
+          .send("❌ Permission Denied! You don't have permissions to view this page  ❌");
+        }
+      })
+    });
+
 
   return router;
 }
