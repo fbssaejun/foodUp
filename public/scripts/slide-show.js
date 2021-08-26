82a447d8cb7b847e870fdc69f6ecd776307e4d37
Anthony Kim <fbssaejun@gmail.com>
2021-08-26 16:18:14 +0000

    eidt layout and design

--- public/scripts/slide-show.js
+++ public/scripts/slide-show.js
@@ -30,21 +30,25 @@ function showSlides(n) {
 
 
 
-//Automatic
-/*
-var slideIndex = 0;
-showSlides();
+// //Automatic
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+// let slideIndex = 0;
+// showSlides();
 
-*/
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "flex";
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
